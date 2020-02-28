24cab7f16e8312bc7d82560feb5cb10a3ad1b009
Lockett Pundt <kalifornium@hotmail.com>
2020-02-27 19:48:10 -0500

    triple joined the review getAll() function to get all of the infor for a review.

--- models/review.js
+++ models/review.js
@@ -4,19 +4,19 @@ const db = require('./conn');
 
 
 class Review {
-    constructor (id, reviewer_id, title, review, restaurant_id) {
+    constructor(id, reviewer_id, title, review, restaurant_id) {
         this.id = id;
-        this.reviewer_id = restaurant_id;
+        this.reviewer_id = reviewer_id;
         this.title = title;
         this.review = review;
         this.restaurant_id = restaurant_id;
     }
     static async getAll() {
         try {
-            const response = await db.any(`SELECT * FROM review JOIN restaurant ON restaurant_id = restaurant.id;`);
+            const response = await db.any(`select reviewer.name as reviewer, restaurant.name, review, title from review JOIN restaurant ON restaurant_id = restaurant.id JOIN reviewer ON reviewer_id = reviewer.id;`);
             console.log(response);
             return response;
-        } catch(error) {
+        } catch (error) {
             return error;
         }
     }
